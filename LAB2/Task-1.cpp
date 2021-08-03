813eecdd6c5fa499436a541d666c45e6c730907c
Roman Molochkov <molochkov2017@gmail.com>
2021-08-03 21:34:41 +0300

    Update Task-1.cpp

--- LAB2/Task-1.cpp
+++ LAB2/Task-1.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector <int> array(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> array[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (array[i] < array[j]) {
+				int temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << array[i] << " ";
+	}
+	return 0;
+}
