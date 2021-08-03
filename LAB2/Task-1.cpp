f8bcb8c27a8c22eec4548395ce9163016e142e99
Roman Molochkov <molochkov2017@gmail.com>
2021-08-03 21:42:28 +0300

    Update Task-1.cpp

--- LAB2/Task-1.cpp
+++ LAB2/Task-1.cpp
@@ -1,30 +1,52 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 
-using namespace std;
+void print_vector(const std::vector<int>& data){
+    for(int i : data){
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
 
-int main() {
-	int n;
-	cin >> n;
+/* Sorting data in ascending order */
+void bubble_sort(std::vector<int>& data){
+  for (int i = 0; i < data.size() - 1; i++) {
+    for (int j = i +1; j < data.size(); j++) {
+      if (data[i] < data[j]) {
+        int temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+      }
+    }
+  }
+}
 
-	vector <int> array(n);
+int main(void){
+    int n = 0;
+    do {
+        std::cout << "Input array size(positive number): ";
+        std::cin >> n;
+    } while(n < 1);
 
-	for (int i = 0; i < n; i++) {
-		cin >> array[i];
-	}
+    /* Reading array */
+    std::vector<int> data(n);
+    for(int i = 0; i < n; ++i){
+        std::cin >> data[i];
+    }
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (array[i] < array[j]) {
-				int temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
-		}
-	}
+    std::cout << "Original data: ";
+    print_vector(data);
 
-	for (int i = 0; i < n; i++) {
-		cout << array[i] << " ";
-	}
-	return 0;
+    auto start = std::chrono::high_resolution_clock::now();
+    bubble_sort(data);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::nanoseconds duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+
+    std::cout << "Data after sorting: ";
+    print_vector(data);
+
+    std::cout << "Time taken: " << duration.count() << "ns" << std::endl;
+
+    return 0;
 }
